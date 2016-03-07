1f3d657e94a20ba56fbfe2d1ff88555be11934d4
Jonathan Huang <jonwhuang@gmail.com>
2016-03-07 01:16:23 -0800

    Remove contact form

--- contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $subject= $_REQUEST['contact-subject'];
-  $sender= $_REQUEST['contact_name'];
-  $email_address = $_REQUEST['contact_email'] ;
-  $message = $_REQUEST['contact_message'] ;
-
-  mail("jonwhuang@gmail.com", $subject, $message, "From: $sender <$email_address>");
-?>
